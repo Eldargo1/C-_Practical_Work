ac6c6d1899c3aa67e40060c0608c85fd320fdf1e
Eldar <kastoom204@gmail.com>
2024-04-21 20:36:39 +0300

    Task1.0.1

--- Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f071ea092ae814614d87676c3b444402e8dd9ee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b538787bbe7057af155f22965b111d125e062e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
