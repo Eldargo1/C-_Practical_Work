79b538787bbe7057af155f22965b111d125e062e
Eldar <kastoom204@gmail.com>
2024-04-21 20:04:50 +0300

    File Task2 modifide added additional info

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишете программу, прогруамму которая принимает
+// целое число N, а на выходе показывает все целые 
+// числа в промежутке от -N до N.
+// Примеры:
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4.
+// 2 => -2, -1, 0, 1, 2.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int negNum = -1 * num; // для того что бы получить негативное число умножаем это число на -1 
+//negNum = -num //либо же перед числом ставим -
+
+while(negNum <= num)
+{
+    Console.Write(negNum+ " ");
+    negNum = negNum + 1; // или можем написать negNum++
+    // negNum = negNum ++; 
+}
+
+
