15b2c6999e46eb906f4461c9f6727730731fd487
Eldar <kastoom204@gmail.com>
2024-04-27 15:37:01 +0300

    Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Заполните массив на N (вводится с консоли, не боее 8 )
+// случайных целых чисел.
+// сформируйте целое число, которе будет состоять из цифр из 
+// массива. Старший разряд числа находятся на 0-м индексе 
+// младший на коследнем.
+
+Console.WriteLine("Введите число");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,10);
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+
+int GetNumInArray()
+{
+    string result = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+       result = result + array[i];
+    }
+    return int.Parse(result);
+    
+}
+
+FillArray();
+PrintArray();
+Console.WriteLine("кол. = " +GetNumInArray());
