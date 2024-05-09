91edd0173fe6b329e3687ec867e6ea8324bcefc3
Eldar <kastoom204@gmail.com>
2024-05-09 17:34:00 +0300

    Task1

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива. 
+
+char [] array = new char[]{'t', '7', ';', 'p'};
+string str = "";
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     str = str + array[i];
+// }
+
+foreach (var item in array)
+{
+    str = str + item;
+}
+Console.WriteLine(str);
