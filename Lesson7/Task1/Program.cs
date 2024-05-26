005eafec4c71a38da23effe8f8f3788a4b647909
Eldar <kastoom204@gmail.com>
2024-05-26 16:53:09 +0300

    Task1

---
+++ Lesson7/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход число и 
+// возвращщать сумму его цифра.
+// Использовать рекурсию.
+// Пример
+// 123 = > 6
+// 69 = > 9
+
+Console.WriteLine("Введите число");
+int userNum = int.Parse(Console.ReadLine());
+
+int result = SetSumNum(userNum);
+Console.WriteLine(result);
+
+int SetSumNum(int num)
+{
+    if(num<= 0)
+    {
+        return 0;
+    }
+    return num%10 + SetSumNum(num/10);
+}
