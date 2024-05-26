0d80947c61155248d9e15a8859250b61ad1b7a98
Eldar <kastoom204@gmail.com>
2024-05-26 17:56:03 +0300

    Task3

---
+++ Lesson7/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Считаать сторку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой сторки.
+// Указание
+// Тспользовать ркекурсию. Не использовать цикл.
+//Пример
+//"Hello" => h l l
+// "World" => w r l d
+//"Hello world" => h l l w r l d
+
+Console.WriteLine("Введите текст");
+string userInput = Console.ReadLine();
+
+int firstEl = 0;
+string vowels = "aeiouy";
+SetSumNum(firstEl);
+
+void SetSumNum(int start)
+{
+    if(start >= userInput.Length)
+    {
+        return;
+    }
+    if(!vowels.Contains(userInput[start]))
+    {
+        Console.Write(userInput[start]);
+    }
+    start++;
+    SetSumNum(start);
+}
